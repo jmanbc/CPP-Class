cf08c8c0f4ac0371ee5439c1f9c75039547bdd79
James Boles <jmboles@arch.fedora>
2024-02-27 00:48:54 -0500

    updates

--- 18.c++CompoundAssignmentOperators/main.cpp
+++ 18.c++CompoundAssignmentOperators/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
-consteval int get_value() { return 3; }
-
 int main() {
-  constexpr int value = get_value();
-  std::cout << " value : " << value << std::endl;
+
+  int value{45};
+
+  std::cout << "The Value is : " << value << std::endl;
+
+  std::cout << std::endl;
+  value += 5; // equivalent to value = value + 5
+
+  std::cout << "The value is (after +=5) : " << value << std::endl; // 50
+
+  std::cout << std::endl;
+  value -= 5; // equivalent to value = value - 5
+  std::cout << "The value is (after -=5) : " << value << std::endl; // 45
+
   return 0;
 }
