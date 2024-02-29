be1a651ba28c3b5fa604c2356042b0aafd429a90
jmboles <james@boleshub.com>
2024-02-28 20:46:09 -0500

    updates

--- 18.c++CompoundAssignmentOperators/main.cpp
+++ 18.c++CompoundAssignmentOperators/main.cpp
@@ -7,13 +7,25 @@ int main() {
   std::cout << "The Value is : " << value << std::endl;
 
   std::cout << std::endl;
-  value += 5; // equivalent to value = value + 5
-
+  value = value += 5;
+  // equivalent to value = value + 5
   std::cout << "The value is (after +=5) : " << value << std::endl; // 50
 
   std::cout << std::endl;
   value -= 5; // equivalent to value = value - 5
   std::cout << "The value is (after -=5) : " << value << std::endl; // 45
+
+  std::cout << std::endl;
+  value *= 2; // equivalent to value = value * 2
+  std::cout << "The value is (after *=2) : " << value * 2 << std::endl; // 90
+
+  std::cout << std::endl;
+  value /= 3; // equivalent to value = value / 3
+  std::cout << "The value is (after /=3) : " << value << std::endl; // 30
+
+  std::cout << std::endl;
+  value %= 11; // equivalent to value = value % 11
+  std::cout << "The value is (after %=11) : " << value << std::endl; // 8
 
   return 0;
 }
